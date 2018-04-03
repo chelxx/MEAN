f4dcf3e62e2c1be8b84e647cc6786ff7b02cb2dc
Chelsea Cabalan <chex239@gmail.com>
2018-04-03 16:13:50 -0500

    MEAN Assignments

---
+++ Node/math-module/app.js
@@ -0,0 +1,5 @@
+var mathlib = require('./mathlib')();
+console.log("This is the add method result:", mathlib.add(5,5));
+console.log("This is the multiply method result:", mathlib.multiply(5, 5));
+console.log("This is the square method result:", mathlib.square(5));
+console.log("This is the random method result:", mathlib.random(5, 5));
