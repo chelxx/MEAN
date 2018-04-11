1d410dafb036eb284ff49fc68e26a61a70c63a7d
Chelsea Cabalan <chex239@gmail.com>
2018-04-11 16:54:59 -0500

    MEAN Assignments

--- Angular/restful-tasks-api/server.js
+++ Angular/restful-tasks-api/server.js
@@ -10,6 +10,7 @@ mongoose.Promise = global.Promise;
 app.use(bodyParser.json());
 
 app.use(express.static(path.join(__dirname, "./static")));
+app.use(express.static( __dirname + '/angular/dist' ));
 app.set("views", path.join(__dirname, "./views"));
 app.set("view engine", "ejs");
 
